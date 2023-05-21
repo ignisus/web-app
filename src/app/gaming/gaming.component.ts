a7ae278805e0f44dd34c0c8fc878d671efe8728b
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-05-21 20:59:31 +0200

    minorFix

--- src/app/gaming/gaming.component.ts
+++ src/app/gaming/gaming.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-gaming',
   templateUrl: './gaming.component.html',
   styleUrls: ['./gaming.component.css']
 })
-export class GamingComponent {
+export class GamingComponent implements OnInit {
+  htmlFileUrl: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+    const filePath = 'assets/ruta_del_archivo.html';
+    this.htmlFileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(filePath);
+  }
 }
